7b0092096f00da247fb0bc2dd488858d63e0fb43
sulekoca <94198611+sulekoca@users.noreply.github.com>
2022-12-13 22:43:06 +0300

    Update Grade.java

--- CSE3063F22_iteration2_classes/Grade.java
+++ CSE3063F22_iteration2_classes/Grade.java
@@ -10,27 +10,4 @@ public class Grade {
         this.grade = grade;
     }
     
-    public CourseSection getCourseSection() {
-        return courseSection;
-    }
-    
-    public void setCourseSection(CourseSection courseSection) {
-        this.courseSection = courseSection;
-    }
-    
-    public Student getStudent() {
-        return student;
-    }
-    
-    public void setStudent(Student student) {
-        this.student = student;
-    }
-    
-    public int getGrade() {
-        return grade;
-    }
-    
-    public void setGrade(int grade) {
-        this.grade = grade;
-    }
 }
