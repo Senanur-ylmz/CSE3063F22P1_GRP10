06003ed07e11b30b5713262a089e101c88ed866f
sulekoca <94198611+sulekoca@users.noreply.github.com>
2022-12-13 22:44:49 +0300

    Update Person.java

--- CSE3063F22_iteration2_classes/Person.java
+++ CSE3063F22_iteration2_classes/Person.java
@@ -8,19 +8,4 @@ public abstract class Person {
             this.address = address;
         }
         
-        public String getName() {
-            return name;
-        }
-        
-        public void setName(String name) {
-            this.name = name;
-        }
-        
-        public String getAddress() {
-            return address;
-        }
-        
-        public void setAddress(String address) {
-            this.address = address;
-        }
     }
