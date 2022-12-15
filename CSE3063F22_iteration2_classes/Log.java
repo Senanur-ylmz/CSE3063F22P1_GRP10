06c179f745c0f5a0c8295f59ffe55165c6675d4d
Senanur-ylmz <80002625+Senanur-ylmz@users.noreply.github.com>
2022-12-15 22:34:57 +0300

    Update Log.java

--- CSE3063F22_iteration2_classes/Log.java
+++ CSE3063F22_iteration2_classes/Log.java
@@ -9,15 +9,29 @@ import org.json.simple.parser.ParseException;
 public class Log {
 private String key;
 
-public Log(String key){
-    this.key=key;
+public Log(){
+  
 }
 
 public String getKey(){
     return key;
 }
+    public static void create_logFile(String semester) throws IOException{  
+   /*The reason it is static is that we get an 
+   error when we define the log in the 
+   courseregistrationsimulation class. */
+    JSONObject jsonObject = new JSONObject();
+       int def = 0;
+       jsonObject.put("SemesterError", def);
+       jsonObject.put("GPAError", def);
+       jsonObject.put("PrerequisiteError", def);
+       jsonObject.put("SeatLimitError", def);
+       FileWriter file = new FileWriter("errorLog_"+semester+".json");
+       file.write(jsonObject.toJSONString());
+       file.close();
+}
 
-    public void logging_error(String semester) throws IOException, ParseException{
+    public void logging_error(String key, String semester) throws IOException, ParseException{
        
             Object obj = new JSONParser().parse(new FileReader("errorLog_"+semester+".json"));
             JSONObject jo = (JSONObject) obj;
@@ -25,7 +39,8 @@ public String getKey(){
            
             int new_value = Integer.parseInt(value) + 1;
            
-            jo.replace(getKey(), new_value);
+            jo.remove(key);
+            jo.put(key, new_value);
           
             FileWriter writer = new FileWriter(("errorLog_"+semester+".json"), false); //overwrites the content of file
             writer.write(jo.toString());
