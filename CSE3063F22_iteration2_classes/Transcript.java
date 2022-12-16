4cbcf949347908f9d3fbc66ef3600079baf2aec6
Senanur-ylmz <80002625+Senanur-ylmz@users.noreply.github.com>
2022-12-17 01:29:09 +0300

    Update Transcript.java

--- CSE3063F22_iteration2_classes/Transcript.java
+++ CSE3063F22_iteration2_classes/Transcript.java
@@ -89,5 +89,55 @@ public class Transcript {
 
 
     }
+    public boolean isCourseNotPassedBefore(Course course){
+        boolean isIt=true;
+        List<Course> student_passed = getSchedule();
+        List<Course> student_transcript = getpassedCourses();
+        String[] passed_before = new String[student_passed.size()];
+        String[] transcript = new String[student_transcript.size()];
+
+        if((student_transcript.size()==0)){
+            isIt=true;
+        }
+    
+        else{
+    
+        for(int i=0;i<student_transcript.size();i++){
+        
+      
+        transcript[i] = String.valueOf(student_transcript.get(i));
+    
+        }
+    
+        int i=0;
+
+        while(i<transcript.length){
+        
+        if((transcript[i].equals(course.getCourseId()))==true){
+            isIt=false;
+        }
+        i++;
+        }
+    
+
+        for(int x=0;x<student_passed.size();x++){
+
+            passed_before[x]=String.valueOf(student_passed.get(x));
+    
+            }
+    
+            int x=0;
+            while(x<passed_before.length){
+              
+            if((passed_before[x].equals(course.getCourseId()))==true){
+                isIt=false;
+            }
+            x++;
+            }
+        }
+
+    return isIt;
+
+    }
     
 }
