736670524fe0021373e1c034b9b0735387efdaa8
Destan22 <52387624+Destan22@users.noreply.github.com>
2023-01-05 00:41:11 +0300

    Add files via upload

---
+++ python_code/Advisor.py
@@ -0,0 +1,10 @@
+import random
+from Student import Student
+class Advisor(object):
+
+    def approve_registration(self, std:Student):
+        std.set_status (self.get_random_boolean(70))
+    
+    def get_random_boolean(self, probability):
+        random_value = random.random() * 100  # 0.0 to 99.9
+        return random_value <= probability
