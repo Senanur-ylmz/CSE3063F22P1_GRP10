aa444efc0aef6a8838b66eb4459b5ef1c6761074
Destan22 <52387624+Destan22@users.noreply.github.com>
2023-01-07 12:31:55 +0300

    Update Person.py

--- python_code/Person.py
+++ python_code/Person.py
@@ -1,15 +1,6 @@
-class Person:
-    def init(self, name:str, address:str):
-        self.__name = name
-        self.__address = address
-    def get_name(self) -> str:
-        return self.__name
-
-    def set_name(self, name:str) -> None:
-        self.__name = name
-
-    def get_address(self) -> str:
-        return self.__address
-
-    def set_address(self, address:str) -> None:
-        self.__address = address
+from abc import ABC, abstractmethod
+class Person(ABC):
+    
+    @abstractmethod 
+    def get_name(self):
+        pass
